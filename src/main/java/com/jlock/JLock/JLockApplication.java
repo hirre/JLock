672fca41929aa8092eb3f69b1d5db032298b5019
Hirad Asadi <hirad.asadi@gmail.com>
2025-03-30 15:43:53 +0200

    first commit

---
+++ src/main/java/com/jlock/JLock/JLockApplication.java
@@ -0,0 +1,13 @@
+package com.jlock.JLock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JLockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlock/JLock/JLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlock.JLock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
