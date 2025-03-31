a953f11355d0d39ef2823ffdd83b14b7a373e95a
Hirad Asadi <hirad.asadi@gmail.com>
2025-03-31 07:53:57 +0200

    refactor LockController to simplify getLock endpoint and update request mapping

--- src/main/java/com/jlock/JLock/controllers/LockController.java
+++ src/main/java/com/jlock/JLock/controllers/LockController.java
@@ -2,13 +2,14 @@ package com.jlock.JLock.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api/locks")
 public class LockController {
 
-    @GetMapping("/api/lock/get-lock")
-    public String getLock(@RequestParam String param) {
+    @GetMapping("/get-lock")
+    public String getLock() {
         return new String();
     }
 }
